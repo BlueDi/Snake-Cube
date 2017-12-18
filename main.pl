f9cbfc3c2f87049742d837a3502adb436eb4de91
Diogo Cruz <diogohmcruz@hotmail.com>
2017-12-18 21:52:51 +0000

    Remake
    A abordagem anterior não era boa.
    Agora sim estou a abordar com restrições.

--- main.pl
+++ main.pl
@@ -1,112 +1,68 @@
 :- use_module(library(clpfd)).
+:- use_module(library(lists)).
+:- use_module(library(fdbg)).
+
 :- dynamic dim/1.
 :- dynamic dim2/1.
+:- dynamic nCubes/1.
 
 snake0([3,2,2,2,2,1,2,2,2,2,2,1,2,2,2,1,2,2,2,2,2,1,2,2,2,2,3]). % ECCCCSCCCCCSCCCSCCCCCSCCCCE   
 snake2([3,2,2,2,2,2,2,3]).
 snake3([3,1,2,2,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,2,3]).
+snake4([3,2,2,2,2,2,2,1,2,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,3]). % Most solutions
+dragons_tail([3,1,2,1,2,1,2,1,2,2,2,2,1,2,1,2,2,2,1,2,2,1,2,2,2,1,3]).
 
 dim(Dim).
-dim2(NCubes).
+dim2(Dim2).
+nCubes(NCubes).
 
 /**
 	Cube values are 1..3, representing Straights, Corners, and Ends.
 	Transitions values are 1..6 standing for the six directions in space where the next cube is, Front, Left, Up, Back, Right, Down respectively.
 */
-snake_cube(Snake, NStraights, NSolutions):-
+snake_cube(Snake):-
 	snake2(Snake),
 	length(Snake, NCubes),
 	Dim is round(NCubes ** (1/3)),
+	Dim2 is round(NCubes ** (1/2)),
 	asserta(dim(Dim)),
-	asserta(dim2(NCubes)),
-	StartPosition in 1..NCubes,
-	set_cube(StartPosition, Cube),
-	
-	domain(Snake, 1, 3),
-	append([3], Tail, Snake),
-	append(Body, [3], Tail),
-	domain(Body, 1, 2),
-	
-	NTransitions is NCubes - 1,
-	length(Transitions, NTransitions),
-	domain(Transitions, 1, 6),
-	
-	duos(Transitions),
-	%trios(Transitions),
-	select_transitions(StartPosition, Body, Cube, Transitions),
-	labeling([], Transitions),
-	write(Transitions).
-	
-set_cube(FirstCube, Cube):-
-	dim2(NCubes),
-	initial_cube(NCubes, InitialCube),
-	!,
-	update_cube(FirstCube, InitialCube, Cube),
-	N1 #= NCubes - 1,
-	global_cardinality(Cube, [1-N1, 2-1]).
+	asserta(dim2(Dim2)),
+	asserta(nCubes(NCubes)),
 
-initial_cube(0, []).
-initial_cube(N, [1 | Cube]):-
-	N1 is N - 1,
-	initial_cube(N1, Cube).
+	nth1(1, Snake, 3),
+	nth1(NCubes, Snake, 3),
 
-select_transitions(_, [], _, [_]).
-select_transitions(SP, [C | Cubes], BigCube, [T1, T2 | Transitions]):-
-	C = 1,
-	T1 #= T2,
-	update_big_cube(SP, T1, BigCube, NP, NewBigCube),
-	select_transitions(NP, Cubes, NewBigCube, [T2 | Transitions]).
-select_transitions(SP, [C | Cubes], BigCube, [T1, T2 | Transitions]):-
-	C = 2,
-	#\ ((T1 #= 1 #\/ T1 #= 4) #/\ (T2 #= 1 #\/ T2 #= 4)),
-	#\ ((T1 #= 2 #\/ T1 #= 5) #/\ (T2 #= 2 #\/ T2 #= 5)),
-	#\ ((T1 #= 3 #\/ T1 #= 6) #/\ (T2 #= 3 #\/ T2 #= 6)),
-	update_big_cube(SP, T1, BigCube, NP, NewBigCube),
-	select_transitions(NP, Cubes, NewBigCube, [T2 | Transitions]).
+	length(Positions, NCubes),
+	domain(Positions, 1, NCubes),
+	all_distinct(Positions),
+	check_transition(Snake, Positions),
+	labeling([], Positions),
+	write(Positions).
 
-/**
-	Updates the state of the final cube.
-*/
-update_big_cube(Position, N, BigCube, NP, NewBigCube):-
+check_transition([3 | Snake], Positions):-
 	dim(Dim),
 	dim2(Dim2),
-	((N #= 1 #/\ NP #= Position + Dim)
-	#\/ (N #= 2 #/\ NP #= Position - 1)
-	#\/ (N #= 3 #/\ NP #= Position + Dim2)
-	#\/ (N #= 4 #/\ NP #= Position - Dim)
-	#\/ (N #= 5 #/\ NP #= Position + 1)
-	#\/ (N #= 6 #/\ NP #= Position - Dim2)),
-	NP #> 0,
-	NP #=< Dim2,
-	update_cube(NP, BigCube, NewBigCube).
-
-update_cube(N, OldCube, NewCube):-
-	dim2(NCubes),
-	append(Head, Body, OldCube),
-	append([1], Tail, Body),
-	length(Head, LH),
-	LH > 0,
-	length(Tail, LT),
-	LT > 0,
-	append(Head, [2], NewHead),
-	append(NewHead, Tail, NewCube),
-	LH #= N - 1 #/\ LT #= NCubes - N,
-	!.
-
-/**
-	In a sequence of 2 transitions there can't be the oposite movement.
-	For example: if the next cube is on the left, after that there can't be a cube on the right.
-*/
-duos([A, B | R]):-
-	#\ (B #= A + 3 #\/ B #= A - 3),
-	duos([B | R]).
-duos([_]).
-
-/**
-	There can't be a sequence of 3 of the same action.
-	With 3 it would leave the area of the cube.
-*/
-trios([A, B, C | R]):-
-	#\ (A #= B #/\ B #= C),
-	trios([B, C | R]).
-trios([_, _]).
+	nCubes(NCubes),
+	First in 1..NCubes,
+	element(First, Positions, 1),
+	element(Second, Positions, 2),
+	Second #= First + 1 #\/ Second #= First - 1 #\/ Second #= First + Dim #\/ Second #= First - Dim #\/ Second #= First + Dim2 #\/ Second #= First - Dim2,
+	Second in 1..NCubes,
+	check_transition(Snake, Positions, First, Second).
+check_transition([3], _, _, _).
+check_transition([Head | Snake], Positions, One, Two):-
+	dim(Dim),
+	dim2(Dim2),
+	nCubes(NCubes),
+	element(One, Positions, N1),
+	element(Two, Positions, N2),
+	N2 #= N1 + 1,
+	N3 #= N2 + 1,
+	Delta #= Two - One,
+	(Head #= 1 #/\ Three #= Two + Delta)
+	#\/ (Head #= 2 #/\ abs(Delta) #= 1 #/\ (Three #= Two + Dim #\/ Three #= Two - Dim #\/ Three #= Two + Dim2 #\/ Three #= Two - Dim2))
+	#\/ (Head #= 2 #/\ abs(Delta) #= Dim #/\ (Three #= Two + 1 #\/ Three #= Two - 1 #\/ Three #= Two + Dim2 #\/ Three #= Two - Dim2))
+	#\/ (Head #= 2 #/\ abs(Delta) #= Dim2 #/\ (Three #= Two + 1 #\/ Three #= Two - 1 #\/ Three #= Two + Dim #\/ Three #= Two - Dim)),
+	Three in 1..NCubes,
+	element(Three, Positions, N3),
+	check_transition(Snake, Positions, Two, Three).
